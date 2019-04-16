e89cd345d26e9b9d37efc8ba99fe4db5b3ba9c84
matttrd <matteoterzi.mt@gmail.com>
2019-04-16 12:09:24 +0200

    attacks

--- code/attack_model.py
+++ code/attack_model.py
@@ -1,45 +1,64 @@
 import torch
 import numpy as np
 import foolbox
-import foolbox.attacks.RandomStartProjectedGradientDescentAttack as PGD
+from foolbox.attacks import RandomStartProjectedGradientDescentAttack as PGD
 import torchnet as tnt 
+import argparse
+import models
+from light_loader import data_ingredient, load_data
+from sacred import Experiment
+from torchnet.meter import ClassErrorMeter, ConfusionMeter
 
-p = argparse.ArgumentParser('unadversarial training',
-                            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-p.add_argument('-i', '--input', type=str, default='', help='location of saved models')
-p.add_argument('--dataset', type=str, default='cifar10', help='dataset')
-p.add_argument('-g', type=int, default=0, help='cuda device')
-p.add_argument('-b', type=int, default=128, help='batch-size')
-p.add_argument('-s', type=int, default=42, help='seed')
+ex = Experiment('attack', ingredients=[data_ingredient])
 
-opt = vars(p.parse_known_args()[0])
+@ex.config
+def cfg():
+    input = ''
+    g = 0
+    b = 1
+    s = 42
+    j = 1
 
+def init_opt():
+    cfg = ex.current_run.config
+    opt = dict()
+    for k,v in cfg.items():
+        opt[k] = v
+    return opt
 
-np.random.seed(opt['s'])
-torch.manual_seed(opt['s'])
-torch.cuda.manual_seed_all(opt['s'])
+@data_ingredient.capture
+def init(name):
+    opt = init_opt()
+    opt['dataset'] = name
+    return opt
 
-
+@ex.automain
 def main():
-	d = th.load(f, map_location=lambda storage, loc: storage.cuda())
-	model = getattr(models, d['arch'])({'d': 0.}).cuda(opt['g'])
-	model.load_state_dict(d['state_dict'])
-	fmodel = foolbox.models.PyTorchModel(model, bounds=(0, 1), num_classes=models.get_num_classes(opt))
-	attack = PGD(fmodel)
-	train_loader, val_loader = load_data(name=opt['dataset'], opt=opt)
-	adv_conf_mat = tnt.meter.ConfusionMeter(models.get_num_classes(opt), normalized=True)
-	errors = ClassErrorMeter(topk=[1])
+    opt=init()
+    np.random.seed(opt['s'])
+    torch.manual_seed(opt['s'])
+    torch.cuda.manual_seed_all(opt['s'])
 
-	for i, (x,y) in enumerate(val_loader):
-		x = x.cuda()
-		y = y.cuda()
-		adversarial = attack(x, y)
-		pred = np.argmax(fmodel.predictions(adversarial))
-		adv_conf_mat.add(pred, y.data.cpu().numpy())
-		errors.add(pred, y.data.cpu().numpy())
-	conf_mat = adv_conf_mat.value()
-	top1 = errors.value()[0]
+    print('Loading model...')
+    d = torch.load(opt['input'], map_location=lambda storage, loc: storage.cuda())
+    opt['d'] = 0.
+    model = getattr(models, d['arch'])(opt).cuda(opt['g'])
+    model.load_state_dict(d['state_dict'])
+    model.eval()
+    fmodel = foolbox.models.PyTorchModel(model, bounds=(0, 1), num_classes=models.get_num_classes(opt))
+    attack = PGD(fmodel, distance=foolbox.distances.Linfinity)
+    train_loader, val_loader, _ = load_data(name=opt['dataset'], opt=opt)
+    adv_conf_mat = ConfusionMeter(models.get_num_classes(opt), normalized=True)
+    errors = ClassErrorMeter(topk=[1])
 
-	print('Error: ', top1)
-	# print('adversarial class', np.argmax(fmodel.predictions(adversarial)))
-	
+    for i, (x,y) in enumerate(val_loader):
+        adversarial = attack(x.cpu().numpy()[0], y.cpu().numpy()[0], epsilon=6./255., binary_search=False)
+        pred = np.argmax(fmodel.predictions(adversarial))
+        pred = torch.from_numpy(pred[np.newaxis, ...]).long()
+        adv_conf_mat.add(pred.data, y.data)
+        #errors.add(pred.data, y.data)
+    conf_mat = adv_conf_mat.value()
+    #top1 = errors.value()[0]
+    print('Error: ', 1-np.trace(conf_mat) / np.sum(conf_mat))
+    # print('adversarial class', np.argmax(fmodel.predictions(adversarial)))
+    
