d7616e8f288f06e6abd42195c1c9e705bebd07fe
mattiacarletti <42202142+mattiacarletti@users.noreply.github.com>
2019-08-13 21:49:47 +0200

    added run file for min dataset exp

---
+++ code/run_MD.sh
@@ -0,0 +1,22 @@
+python main_lt.py with epochs=30 wd=5e-4 lrs='[[0,0.1],[10,0.01],[20,0.001]]' pilot=True save=True
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.1 
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.2
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.3
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.4
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.5
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.6
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.7
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.1 dataset.mode=1
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.2 dataset.mode=1 
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.3 dataset.mode=1
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.4 dataset.mode=1
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.5 dataset.mode=1
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.6 dataset.mode=1
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.7 dataset.mode=1
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.1 dataset.mode=2
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.2 dataset.mode=2 
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.3 dataset.mode=2
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.4 dataset.mode=2
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.5 dataset.mode=2
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.6 dataset.mode=2
+python main_lt.py with save=True wd=5e-4 arch='resnet18' dataset.perc=0.7 dataset.mode=2
