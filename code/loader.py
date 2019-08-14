dc38818c1b351931b5c64f69e65532050b15ce4e
matttrd <matteoterzi.mt@gmail.com>
2019-08-14 16:50:27 +0200

    minor

--- code/loader.py
+++ code/loader.py
@@ -191,7 +191,7 @@ def load_data(name, source, shuffle, frac, perc, mode, pilot_samp, pilot_arch, n
     num_classes = get_num_classes(opt)
     if perc > 0:
         print('Dataset reduction of ', perc)
-        sd_idx = np.squeeze(torch.load('sorted_idx_' + name + '_' + pilot_samp + '.pz')) 
+        sd_idx = np.squeeze(torch.load('sorted_idx_' + name + '_' + pilot_arch + '_' + pilot_samp + '.pz')) 
         if mode == 0:
             idx = sd_idx[int((1 - perc) * train_length):]
         elif mode == 1:
