03eb78f7afecf6b1044e093ba184217e5729b656
Luca Zancato <zancato.luca@gmail.com>
2019-10-30 12:07:49 +0100

    Added the organizer script! Choose and experiment and every .pdf .log will be placed in a folder called: analysis_experiment. Inside the experiment folder.

---
+++ code/organize_experiment_analysis.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+from IPython import embed
+import glob2
+from shutil import copyfile
+
+
+def get_paths_to_be_saved(run):
+    files_to_be_saved = []
+    files_to_be_saved += glob2.glob(f'{run}/**/*.log')
+    files_to_be_saved += glob2.glob(f'{run}/**/*.pdf')
+    return files_to_be_saved
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Options")
+    parser.add_argument('--input_dir', type=str, required=True) # Choose the experiment
+    parser.add_argument('--output_dir', type=str, default=None)
+    # parser.add_argument(
+    #     '--output_name',
+    #     type=str,
+    #     required=True)
+    # parser.add_argument('--epochs', type=int, default=200)
+    args = parser.parse_args()
+
+    exp_path =  os.path.join(f'..{os.sep}results{os.sep}', args.input_dir)
+
+    if not args.output_dir:
+        save_path =os.path.join(exp_path, 'analysis_experiments')
+
+    # embed()
+
+    # Get all run paths you need to get logs and pdf (general things to be saved)
+    for i, run in enumerate(os.listdir(exp_path)):
+        save_run_path = os.path.join(save_path, str(i))
+        os.makedirs(save_run_path, exist_ok=True)
+        if not run == 'analysis_experiments':
+            for file in get_paths_to_be_saved(os.path.join(exp_path, run)):
+                copyfile(file, os.path.join(save_run_path, file.split(f'{os.sep}')[-1]))
+
+
+    # embed()
