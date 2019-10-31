583ffbeb6ca192db931ab04710aa6109195a075c
mattiacarletti <42202142+mattiacarletti@users.noreply.github.com>
2019-10-31 01:51:53 +0100

    updated visualization_module

--- code/new_data_viz.py
+++ code/new_data_viz.py
@@ -19,14 +19,15 @@ if __name__ == '__main__' :
     parser.add_argument('--arch', required=True, nargs='*', help='List of architectures to be considered ("resnet18", "resnet34")')
     parser.add_argument('--datasets', required=True, nargs='*', help='List of datasets to be considered ("cifar10", "cifar100", "imagenet_lt")')
     # quantity to plot on the y-axis (plot) and phase
-    parser.add_argument('--plot', required=True, help='Plot to be created ("loss", "top1")')
-    parser.add_argument('--phase', required=True, help='Phase to plot ("train", "val", "train_val")')
+    parser.add_argument('--plot', required=True, help='Plot to be created ("loss", "top1", "class_count")')
+    parser.add_argument('--phase', help='Phase to plot ("train", "val", "train_val")')
+    parser.add_argument('--epochs', nargs='*', help='List of epochs to be considered (for class count hist)')
     # comparison at plot level (grouping variables)
-    parser.add_argument('--hue', default=None, help='Hue grouping variable for the seaborn plot')
-    parser.add_argument('--style', default=None, help='Style grouping variable for the seaborn plot (ignored when using phase="train_val")')
-    parser.add_argument('--size', default=None, help='Size grouping variable for the seaborn plot')
+    parser.add_argument('--hue', default=None, type=json.loads, help='Hue grouping variable for the seaborn plot')
+    parser.add_argument('--style', default=None, type=json.loads, help='Style grouping variable for the seaborn plot (ignored when using phase="train_val")')
+    parser.add_argument('--size', default=None, type=json.loads, help='Size grouping variable for the seaborn plot')
     # comparison at file level
-    parser.add_argument('--cfl', default=None, nargs='*', help='List of variables for comparisons at file level')
+    parser.add_argument('--cfl', default=None, type=json.loads, help='List of variables for comparisons at file level')
     opt = vars(parser.parse_args())
     
 
@@ -34,20 +35,28 @@ if __name__ == '__main__' :
     if opt['phase'] == 'train_val':
         opt['style'] = None
         print('\n"style" argument ignored since "phase"=="train_val"\n')
+    # when plot='class_count', opt['style'] and opt['size'] cannot be used
+    if opt['plot'] == 'class_count' and opt['style'] is not None and opt['size'] is not None:
+        opt['style'] = None
+        opt['size'] = None
+        print('\n"style" and "size" arguments cannot be used since "plot"=="class_count"\n')
     # if 'arch' and 'dataset' are not grouping variables,
     # information about the architecture and dataset must be included in 'cfl'
-    if 'arch' not in [opt['hue'],opt['style'],opt['size']]:
+    options = [list(opt[name].keys())[0] for name in ['hue','style','size'] if opt[name]]
+    if 'arch' not in options:
         assert opt['cfl'] is not None, "'arch' must be added to the variables for comparison at file level" 
-        assert 'arch' in opt['cfl'], "'arch' must be added to the variables for comparison at file level"
-    if 'dataset' not in [opt['hue'],opt['style'],opt['size']]:
+        assert 'arch' in opt['cfl'].keys(), "'arch' must be added to the variables for comparison at file level"
+    if 'dataset' not in options:
         assert opt['cfl'] is not None, "'dataset' must be added to the variables for comparison at file level"
-        assert 'dataset' in opt['cfl'], "'dataset' must be added to the variables for comparison at file level"
+        assert 'dataset' in opt['cfl'].keys(), "'dataset' must be added to the variables for comparison at file level"
     # PLOTS
     if opt['plot'] in ['loss', 'top1']:
         if opt['phase'] in ['train','val']:
             viz.plot(opt)
         elif opt['phase'] == 'train_val':
             viz.plot_train_val(opt)
+    elif opt['plot'] == 'class_count':
+        viz.class_count_hist(opt)
     else:
         NotImplementedError
 
--- code/visualization_module.py
+++ code/visualization_module.py
@@ -4,14 +4,18 @@ import pandas as pd
 import os, json
 import pdb
 import matplotlib.ticker as ticker
+import matplotlib.animation as animation
 import glob2
 import itertools
-
+import numpy as np
+import re
+import pickle
 
 sns.set('paper')
-colors = ["windows blue", "amber", "greyish", "faded green", "dusty purple"]
 sns.set_color_codes()
-colors = ["windows blue", "red", "amber", "greyish", "faded green", "dusty purple"]
+colors = ["tab:blue","tab:orange","tab:green","tab:red","tab:purple","tab:brown",
+          "tab:pink","tab:gray","tab:olive","tab:cyan"] 
+#colors = ['b','r','g','c']
 
 blacklist = ['lrs', 'B', 'b', 'd', 's', 'ids', 'd', \
             'save', 'metric', 'nc', 'g', 'j', 'env', 'burnin', \
@@ -93,6 +97,8 @@ def get_data_loss_top1(opt):
         ex = []
         # remove pilots
         for f in expts:
+            if f.split('/')[-3] == 'analysis_experiments':
+                continue
             fstr = '{' + f.split('{')[1].split('}')[0] + '}'
             keywords = json.loads(fstr)
             if 'pilot' not in keywords.keys():
@@ -112,32 +118,31 @@ def get_data_loss_top1(opt):
             if 'use_train_clean' not in keywords.keys():
                 keywords['use_train_clean'] = False
                 kws['use_train_clean'] = keywords['use_train_clean']
-            if opt['hue'] is not None and opt['hue'] not in keywords.keys():
-                keywords[opt['hue']] = get_default_value(opt['hue'])
-                kws[opt['hue']] = keywords[opt['hue']]
-            if opt['style'] is not None and opt['style'] not in keywords.keys():
-                keywords[opt['style']] = get_default_value(opt['style'])
-                kws[opt['style']] = keywords[opt['style']]
-            if opt['size'] is not None and opt['size'] not in keywords.keys():
-                keywords[opt['size']] = get_default_value(opt['size'])
-                kws[opt['size']] = keywords[opt['size']]
+            if opt['hue'] is not None and list(opt['hue'].keys())[0] not in keywords.keys():
+                keywords[list(opt['hue'].keys())[0]] = get_default_value(list(opt['hue'].keys())[0])
+                kws[list(opt['hue'].keys())[0]] = keywords[list(opt['hue'].keys())[0]]
+            if opt['style'] is not None and list(opt['style'].keys())[0] not in keywords.keys():
+                keywords[list(opt['style'].keys())[0]] = get_default_value(list(opt['style'].keys())[0])
+                kws[list(opt['style'].keys())[0]] = keywords[list(opt['style'].keys())[0]]
+            if opt['size'] is not None and list(opt['size'].keys())[0] not in keywords.keys():
+                keywords[list(opt['size'].keys())[0]] = get_default_value(list(opt['size'].keys())[0])
+                kws[list(opt['size'].keys())[0]] = keywords[list(opt['size'].keys())[0]]
             if opt['cfl'] is not None:
-                for name in opt['cfl']:
+                for name in opt['cfl'].keys():
                     if name not in keywords.keys():
                         keywords[name] = get_default_value(name)
                         kws[name] = keywords[name]
             # load data
             di = loadlog(f, kws=kws)
             d.append(di)
-
     df = pd.concat(d)
     whitelist = ['use_train_clean','train_clean','train','val','arch','dataset','e','loss', 'top1']
     if opt['cfl'] is not None:
-        for param in opt['cfl']:
+        for param in opt['cfl'].keys():
             whitelist.append(param)
     for param in [opt['hue'],opt['style'],opt['size']]:
         if param is not None:
-            whitelist.append(param)
+            whitelist.append(list(param.keys())[0])
     # remove duplicates
     whitelist = list(set(whitelist))
 
@@ -158,10 +163,68 @@ def get_data_loss_top1(opt):
 
     return pd.concat([df_train, df_val])
 
+def get_data_counts(opt):
+    # list of source directories
+    source_dir = ['{}{}_{}_{}'.format(opt['base'],opt['exp'],pair[0],pair[1]) for pair in list(itertools.product(opt['arch'], opt['datasets']))]
+    d = []
+    max_count = 0
+    for dir_ in source_dir:
+        expts = glob2.glob(f'{dir_}/**/sample_counts')
+        ex = []
+        # remove pilots
+        for f in expts:
+            fstr = '{' + f.split('{')[1].split('}')[0] + '}'
+            keywords = json.loads(fstr)
+            if 'pilot' not in keywords.keys():
+                ex.append(f)
+        for f in ex:
+            fstr = '{' + f.split('{')[1].split('}')[0] + '}'
+            keywords = json.loads(fstr)
+            # default settings
+            if 'arch' not in keywords.keys():
+                keywords['arch'] = 'resnet10'
+            if 'dataset' not in keywords.keys():
+                keywords['dataset'] = 'cifar10'
+            if 'use_train_clean' not in keywords.keys():
+                keywords['use_train_clean'] = False
+            if opt['hue'] is not None and list(opt['hue'].keys())[0] not in keywords.keys():
+                keywords[list(opt['hue'].keys())[0]] = get_default_value(list(opt['hue'].keys())[0])
+            if opt['style'] is not None and list(opt['style'].keys())[0] not in keywords.keys():
+                keywords[list(opt['style'].keys())[0]] = get_default_value(list(opt['style'].keys())[0])
+            if opt['size'] is not None and list(opt['size'].keys())[0] not in keywords.keys():
+                keywords[list(opt['size'].keys())[0]] = get_default_value(list(opt['size'].keys())[0])
+            if opt['cfl'] is not None:
+                for name in opt['cfl'].keys():
+                    if name not in keywords.keys():
+                        keywords[name] = get_default_value(name)
+            # load data
+            train_labels = np.load('./train_labels_' + keywords['dataset'] + '.npy')
+            num_classes = len(np.unique(train_labels))
+            data = []
+            for e in opt['epochs']:
+                with open(os.path.join(f,'sample_counts_' + str(e) + '.pkl'), 'rb') as file:
+                    counts = pickle.load(file)
+                class_count = np.zeros(10)
+                for sample_id in range(counts.shape[0]):
+                    current_class = train_labels[sample_id]
+                    class_count[current_class] += counts[sample_id]
+                total_count = np.sum(class_count)
+                #class_count = class_count / total_count
+                data.append(class_count)
+            keywords['data'] = data
+            di = pd.DataFrame(keywords)
+            d.append(di)
+            current_max_count = np.max(data[-1])
+            if current_max_count > max_count:
+                max_count = current_max_count
+    df = pd.concat(d)  
+
+    return df, max_count, num_classes
+
 def get_filename(keys, values, opt):
     # name of the "comparison at plot level" part
     cpl = [opt['hue'], opt['style'], opt['size']]
-    fn_cpl = '{'+','.join(item for item in cpl if item)+'}'
+    fn_cpl = '{'+','.join(list(item.keys())[0] for item in cpl if item)+'}'
     # name of the "comparison at file level" part
     fn_cfl = []
     for i in range(len(keys)):
@@ -171,16 +234,61 @@ def get_filename(keys, values, opt):
     fn = fn_cpl+'_'+fn_cfl
     return fn
 
+def get_filename_clean(keys, values, opt):
+    fn = []
+    for i in range(len(keys)):
+        fn.append(':'.join([keys[i],str(values[i])]))
+    fn = '__'.join(fn)
+    fn = fn.replace('normalizer:True','normalizer')
+    fn = fn.replace('normalizer:False','')
+    fn = fn.replace(':','_')
+    for word in ['sampler_','arch_','dataset_']:
+        fn = fn.replace(word,'')
+    fn = fn.replace('temperature', 'temp')
+    fn = fn.replace('.', 'point')
+    while fn.endswith('_'):
+        fn = fn[:-2]
+    return fn
+
 def plot(opt):
+    options = [opt[name] for name in ['hue','style','size'] if opt[name]]
+    plot_opt = []
+    for name in ['hue','style','size']:
+        if opt[name]:
+            plot_opt.append(list(opt[name].keys())[0])
+        else:
+            plot_opt.append(opt[name])
     # load data
     df = get_data_loss_top1(opt)
     # filter by phase (train or val)
     df = df[df.phase==opt['phase']]
-    # create dict with values assumed by variables in opt['cfl']
+    # consider only specified values for opt['hue'], opt['size'], opt['size']
+    for option in options:
+        key = list(option.keys())[0]
+        if option[key] == 'all':
+            # use all values and do not filter
+            print('Keyword {}: using all values'.format(key))
+        else:
+            # filter out non-selected values
+            selected_values = []
+            for elem in option[key]:
+                selected_values.append(elem)
+            df = df[df[key].isin(selected_values)] 
+            print('Keyword {}: using values {}'.format(key, selected_values))
+    # create dict with selected values assumed by variables in opt['cfl']
     cfl = {}
     if opt['cfl'] is not None:
-        for el in opt['cfl']:
-            cfl[el] = df[el].unique()
+        for key in opt['cfl'].keys():
+            print('Keyword {}: using '.format(key), end='')
+            if opt['cfl'][key] == 'all':
+                print('all values')
+                selected_values = df[key].unique()
+            else:
+                selected_values = []
+                for elem in opt['cfl'][key]:
+                    print('{} '.format(elem), end='')
+                    selected_values.append(elem)
+            cfl[key] = selected_values
     # create a plot for every combination of values assumed by variables in opt['cfl'] (comparisons at file level)
     keys = list(cfl.keys())
     for values in list(itertools.product(*cfl.values())):
@@ -188,27 +296,61 @@ def plot(opt):
         for i in range(len(keys)):
             dfc = dfc[dfc[keys[i]]==values[i]]
         plt.clf()
-        ax = sns.lineplot(x='e', y=opt['plot'], hue=opt['hue'], style=opt['style'], size=opt['size'], data=dfc)
+        if opt['hue'] is not None:
+            palette = [colors[i] for i in range(dfc[plot_opt[0]].nunique())]
+        else:
+            palette=None
+        ax = sns.lineplot(x='e', y=opt['plot'], hue=plot_opt[0], style=plot_opt[1], size=plot_opt[2], palette=palette, data=dfc)
         fm = ticker.ScalarFormatter()
         fm.set_scientific(False)
         ax.yaxis.set_major_formatter(fm)
         ax.yaxis.set_minor_formatter(fm)
         ax.set(xlabel='epoch', ylabel=opt['phase'] + ' ' + opt['plot'])
-        dir_ = os.path.join(opt['sd'], opt['exp'])
+        #dir_ = os.path.join(opt['sd'], opt['exp'])
+        dir_ = os.path.join(opt['sd'], 'loss_acc')
         if not os.path.isdir(dir_):
             os.makedirs(dir_)
-        fn = get_filename(keys, values, opt)
-        plt.savefig(os.path.join(dir_, opt['plot'] + '_' + opt['phase'] + '_' + fn + '.pdf'), bbox_inches='tight', format='pdf')
+        fn = get_filename_clean(keys, values, opt)
+        plt.savefig(os.path.join(dir_, opt['plot'] + '_' + opt['phase'] + '__' + fn + '.pdf'), bbox_inches='tight', format='pdf')
         plt.close()
 
 def plot_train_val(opt):
+    options = [opt[name] for name in ['hue','style','size'] if opt[name]]
+    plot_opt = []
+    for name in ['hue','style','size']:
+        if opt[name]:
+            plot_opt.append(list(opt[name].keys())[0])
+        else:
+            plot_opt.append(opt[name])
     # load data
     df = get_data_loss_top1(opt)
-    # create dict with values assumed by variables in opt['cfl']
+    # consider only specified values for opt['hue'], opt['size'], opt['size']
+    for option in options:
+        key = list(option.keys())[0]
+        if option[key] == 'all':
+            # use all values and do not filter
+            print('Keyword {}: using all values'.format(key))
+        else:
+            # filter out non-selected values
+            selected_values = []
+            for elem in option[key]:
+                selected_values.append(elem)
+            df = df[df[key].isin(selected_values)] 
+            print('Keyword {}: using values {}'.format(key, selected_values))
+    # create dict with selected values assumed by variables in opt['cfl']
     cfl = {}
     if opt['cfl'] is not None:
-        for el in opt['cfl']:
-            cfl[el] = df[el].unique()
+        for key in opt['cfl'].keys():
+            print('Keyword {}: using '.format(key), end='')
+            if opt['cfl'][key] == 'all':
+                print('all values')
+                selected_values = df[key].unique()
+            else:
+                selected_values = []
+                for elem in opt['cfl'][key]:
+                    print('{} '.format(elem), end='')
+                    selected_values.append(elem)
+            cfl[key] = selected_values
     # create a plot for every combination of values assumed by variables in opt['cfl'] (comparisons at file level)
     keys = list(cfl.keys())
     for values in list(itertools.product(*cfl.values())):
@@ -216,16 +358,97 @@ def plot_train_val(opt):
         for i in range(len(keys)):
             dfc = dfc[dfc[keys[i]]==values[i]]
         plt.clf()
-        ax = sns.lineplot(x='e', y=opt['plot'], hue=opt['hue'], style='phase', size=opt['size'], data=dfc)
+        if opt['hue'] is not None:
+            palette = [colors[i] for i in range(dfc[plot_opt[0]].nunique())]
+        else:
+            palette=None
+        ax = sns.lineplot(x='e', y=opt['plot'], hue=plot_opt[0], style='phase', size=plot_opt[2], palette=palette, data=dfc)
         fm = ticker.ScalarFormatter()
         fm.set_scientific(False)
         ax.yaxis.set_major_formatter(fm)
         ax.yaxis.set_minor_formatter(fm)
         ax.set(xlabel='epoch', ylabel=opt['plot'])
-        dir_ = os.path.join(opt['sd'], opt['exp'])
+        #dir_ = os.path.join(opt['sd'], opt['exp'])
+        dir_ = os.path.join(opt['sd'], 'loss_acc')
         if not os.path.isdir(dir_):
             os.makedirs(dir_)
-        fn = get_filename(keys, values, opt)
-        plt.savefig(os.path.join(dir_, opt['plot'] + '_train_val_' + fn + '.pdf'), bbox_inches='tight', format='pdf')
+        fn = get_filename_clean(keys, values, opt)
+        plt.savefig(os.path.join(dir_, opt['plot'] + '_train_val__' + fn + '.pdf'), bbox_inches='tight', format='pdf')
         plt.close()
+
+def class_count_hist(opt):
+    # load data
+    df, max_count, num_classes = get_data_counts(opt)
+    # consider only specified values for opt['hue'], opt['size'], opt['size']
+    if opt['hue'] is not None:
+        key = list(opt['hue'].keys())[0]
+        if opt['hue'][key] == 'all':
+            # use all values and do not filter
+            print('Keyword {}: using all values'.format(key))
+        else:
+            # filter out non-selected values
+            selected_values = []
+            for elem in opt['hue'][key]:
+                selected_values.append(elem)
+            df = df[df[key].isin(selected_values)] 
+            print('Keyword {}: using values {}'.format(key, selected_values))
+    # create dict with selected values assumed by variables in opt['cfl']
+    cfl = {}
+    if opt['cfl'] is not None:
+        for key in opt['cfl'].keys():
+            print('Keyword {}: using '.format(key), end='')
+            if opt['cfl'][key] == 'all':
+                print('all values')
+                selected_values = df[key].unique()
+            else:
+                selected_values = []
+                for elem in opt['cfl'][key]:
+                    print('{} '.format(elem), end='')
+                    selected_values.append(elem)
+            cfl[key] = selected_values
+    # create a plot for every combination of values assumed by variables in opt['cfl'] (comparisons at file level)
+    keys = list(cfl.keys())
+    for values in list(itertools.product(*cfl.values())):
+        dfc = df.copy()
+        for i in range(len(keys)):
+            dfc = dfc[dfc[keys[i]]==values[i]]
+        plt.clf()
+        if opt['hue'] is not None:
+            hue_values = dfc[list(opt['hue'].keys())[0]].unique()
+            palette = [colors[i] for i in range(len(hue_values))]
+        else:
+            palette=None
+        data = dfc.data.values
+        epochs = opt['epochs']
+        number_of_frames = len(epochs)
+
+        def update_hist(i):
+            plt.cla()
+            idx = np.asarray([i for i in range(num_classes)])
+            ax.set_xticks(idx)
+            ax.set_xticklabels(idx)
+            ax.set_xlabel('Class')
+            ax.set_title('After {} epochs'.format(str(int(epochs[i])+1)))
+            ax.set_ylim(top=max_count+0.05*max_count)
+            ax.set_ylabel('Count')
+            if opt['hue'] is not None:
+                width = 0.2
+                plots = []
+                for j, hue_val in enumerate(hue_values):
+                    dfc_current = dfc[dfc[list(opt['hue'].keys())[0]]==hue_val]
+                    data = dfc_current.data.values
+                    curr_plot = ax.bar(np.arange(0,num_classes)+j*width, data[i], color=palette[j], width=width)
+                    plots.append(curr_plot[0])
+                ax.legend(plots, hue_values, title=list(opt['hue'].keys())[0])
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        anim = animation.FuncAnimation(fig, update_hist, number_of_frames, interval=1000, repeat=False)
+        dir_ = os.path.join(opt['sd'], 'class_count')
+        if not os.path.isdir(dir_):
+            os.makedirs(dir_)
+        fn = get_filename_clean(keys, values, opt)
+        anim.save(os.path.join(dir_, opt['plot'] + '__' + fn + '.gif'), writer='imagemagick')
+
+
 
