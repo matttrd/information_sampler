0c196b46f6817aed60f3ee43ee474571edeb4005
Luca Zancato <zancato.luca@gmail.com>
2019-11-04 04:10:55 +0100

    Added new plots comparing noisy data inside the counts

--- code/check_masks.py
+++ code/check_masks.py
@@ -33,8 +33,6 @@ class MyDataset(Dataset):
 
 
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
@@ -64,7 +62,7 @@ def print_mask(path):
         count[train_dataset[index][1]] += 1
 
     print(count)
-    return count
+    return count, corr_indeces
 
 parser = argparse.ArgumentParser(description='check_masks',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -72,8 +70,108 @@ parser.add_argument('--exp', default=f'..{os.sep}results', required=True)
 opt = vars(parser.parse_args())
 
 counts = []
+corr_indeces = []
 exp_path = os.path.join(f'..{os.sep}results', opt['exp'])
 for runs in os.listdir(exp_path):
     for file in os.listdir(os.path.join(exp_path, runs)):
         if file.split('_')[0] == 'indices':
-            counts.append(print_mask(os.path.join(exp_path, runs, file)))
+            count, corr_ind = print_mask(os.path.join(exp_path, runs, file))
+            counts.append(count)
+            corr_indeces.append(corr_ind)
+
+import matplotlib.pyplot as plt
+def print_perc(save_path, info, IS_counts, corr_indeces):
+    os.makedirs(save_path, exist_ok=True)
+    epoch_filer = [0, 61, 121, 179]
+    title = info['sampler'] + '_' + 'Temp' + '_' + str(info['temperature']).replace('.', '_') + '_' + 'norm_' + str(info['norm'])
+    for epoch in IS_counts.keys():
+        if int(epoch) in epoch_filer:
+            indeces = np.argsort(IS_counts[epoch])[::-1]
+            outlier_perc = dict()
+            for i, perc in enumerate(range(1000, len(indeces)+ 501, 500)):
+                mask = np.isin(indeces[:perc], corr_indeces)
+                outlier_perc[perc] = len(indeces[:perc][mask])/len(indeces[:perc])
+
+            p_count, p_sampled = [], []
+            for key, value in outlier_perc.items():
+                p_count.append(key)
+                p_sampled.append(value)
+
+            plt.plot(p_count, p_sampled, label= 'Epoch ' + epoch)
+    plt.legend(), plt.grid(), plt.title(title), plt.tight_layout()
+    plt.savefig(os.path.join(save_path, f'{title}.pdf'), dpi=2560, bbox_inches='tight')
+    plt.close()
+
+def print_hist(save_path, info, IS_counts, corr_indeces):
+    os.makedirs(save_path, exist_ok=True)
+    epoch_filer = [0, 61, 121, 179]
+    title = info['sampler'] + '_' + 'Temp' + '_' + str(info['temperature']).replace('.', '_') + '_' + 'norm_' + str(info['norm'])
+    fig, axs = plt.subplots(1, len(epoch_filer))
+    fig.set_size_inches(18.5, 10.5)
+    for epoch in IS_counts.keys():
+        if int(epoch) in epoch_filer:
+            i = int(np.where(epoch_filer == np.array(int(epoch)))[0])
+            counts_removed_corr = np.delete(IS_counts[epoch], corr_indeces)
+            counts_corr = IS_counts[epoch][corr_indeces]
+            axs[i].hist(counts_removed_corr, label='Plain labels')
+            axs[i].hist(counts_corr, label='Corrupted labels')
+            axs[i].set_title('Epoch ' + epoch)
+            # plt.legend()
+            plt.grid()
+            i+=1
+    plt.legend(), plt.title(title), plt.tight_layout()
+    plt.savefig(os.path.join(save_path, f'distribution_{title}.pdf'))
+    # plt.show()
+    plt.close()
+
+from count_visualization import get_params_from_log
+# Gather counts
+info_exps = []
+list_IS_counts = []
+i = 0
+for runs in os.listdir(exp_path):
+    if runs.split(f'{os.sep}')[-1] == 'analysis_experiments':
+        i -= 1
+        continue
+    IS_counts = {}
+    info_exps.append(get_params_from_log(os.path.join(exp_path, runs, 'logs', 'flogger.log')))
+    for file in os.listdir(os.path.join(exp_path, runs, 'sample_counts')):
+        counts_path = os.path.join(exp_path, runs, 'sample_counts', file)
+        epoch = file.split('_')[-1].split('.')[0]
+        f = open(counts_path, 'rb')
+        IS_counts[epoch] = pkl.load(f)
+
+    # Print histogram of counts for outliers
+    print_perc(os.path.join(exp_path, runs, 'count_distr_noisy'), info_exps[-1], IS_counts, corr_indeces[i])
+    print_hist(os.path.join(exp_path, runs, 'count_distr_noisy'), info_exps[-1], IS_counts, corr_indeces[i])
+
+    list_IS_counts.append(IS_counts)
+    i+= 1
+
+# Plot print_perc of a set of experiment you can choose using: run_to_pot
+epoch_filer = [0, 61, 121, 179]
+run_to_plot = [i for i in range(20)]
+plt.figure()
+for run, IS_counts in enumerate(list_IS_counts):
+    if run in run_to_plot:
+        for epoch in IS_counts.keys():
+            if int(epoch) in epoch_filer:
+                indeces = np.argsort(IS_counts[epoch])[::-1]
+                outlier_perc = dict()
+                for i, perc in enumerate(range(1000, len(indeces)+ 501, 500)):
+                    mask = np.isin(indeces[:perc], corr_indeces[run])
+                    outlier_perc[perc] = len(indeces[:perc][mask])/len(indeces[:perc])
+
+                p_count, p_sampled = [], []
+                for key, value in outlier_perc.items():
+                    p_count.append(key)
+                    p_sampled.append(value)
+
+                plt.plot(p_count, p_sampled, label=epoch + ' ' + str(run))
+plt.legend()
+save_path = os.path.join(exp_path, 'analysis_experiments')
+os.makedirs(save_path, exist_ok=True)
+plt.savefig(os.path.join(save_path, f'comparison_runs_noisy_distr.pdf'), dpi=2560, bbox_inches='tight')
+
+
+# Gather forgetting indeces
