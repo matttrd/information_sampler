7769beb950fce75152508d3b62db77358428e2a2
LucaZancato <45417537+LucaZancato@users.noreply.github.com>
2019-11-07 13:13:50 +0100

    added print zanca

--- code/print_zanca.py
+++ code/print_zanca.py
@@ -1,13 +1,14 @@
 import argparse
 import os                                                                                                                                                                                         parser = argparse.ArgumentParser(description='count_visualization',
 
-parser = argparse.ArgumentParser(description='count_visualization', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(description='count_visualization',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--exp', default=f'..{os.sep}results', required=True)
 opt = vars(parser.parse_args())
 
 os.system(f'python3 count_visualization.py --exp ' + opt['exp'])
-os.system(f'python3 print_outliers.py --exp ' + opt['exp'])                                      
-if 'corr' in opt['exp']:                                                                             
+os.system(f'python3 print_outliers.py --exp ' + opt['exp'])
+if 'corr' in opt['exp']:
     os.system(f'python3 check_masks.py --exp ' + opt['exp'])
 
-os.system(f'pyhton3 organize_experiment_analysis.py --exp ' + opt['exp'])     
+os.system(f'pyhton3 organize_experiment_analysis.py --exp ' + opt['exp'])
