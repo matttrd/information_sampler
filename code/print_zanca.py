51e2d82f89b0d43184b52097da761a48b9518543
Luca Zancato <zancato.luca@gmail.com>
2019-11-07 14:31:23 +0100

    corrected priniting

--- code/print_zanca.py
+++ code/print_zanca.py
@@ -1,5 +1,6 @@
+from IPython import embed
 import argparse
-import os                                                                                                                                                                                         parser = argparse.ArgumentParser(description='count_visualization',
+import os
 
 parser = argparse.ArgumentParser(description='count_visualization',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -7,8 +8,10 @@ parser.add_argument('--exp', default=f'..{os.sep}results', required=True)
 opt = vars(parser.parse_args())
 
 os.system(f'python3 count_visualization.py --exp ' + opt['exp'])
-os.system(f'python3 print_outliers.py --exp ' + opt['exp'])
+#os.system(f'python3 print_outliers.py --exp ' + opt['exp'])
 if 'corr' in opt['exp']:
     os.system(f'python3 check_masks.py --exp ' + opt['exp'])
 
-os.system(f'pyhton3 organize_experiment_analysis.py --exp ' + opt['exp'])
+os.system(f'python3 organize_experiment_analysis.py --exp ' + opt['exp'])
+
+
