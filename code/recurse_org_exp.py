cce881987f923c076d9ed8ab5c99d0c6cea557e9
Luca Zancato <zancato.luca@gmail.com>
2019-11-12 00:22:42 +0100

    now you are not copying all the experiments!

--- code/recurse_org_exp.py
+++ code/recurse_org_exp.py
@@ -7,13 +7,13 @@ parser.add_argument('--dropbox', default=False)
 opt = vars(parser.parse_args())
 
 path = '../results'
-folders = glob2.glob('../results/CVPR*')
+folders = glob2.glob("../results/CVPR*")
 
 for exp_path in folders:
     #exp_path = os.path.join(path,exp)
     exp = exp_path.split('/')[-1]
-    os.system(f"python organize_experiment_analysis.py --exp {exp}")
+    os.system(f"python3 organize_experiment_analysis.py --exp {exp}")
 
     if opt['dropbox']:
-        os.system(f"cp -r {exp_path} ~/Dropbox/tmp_results/{exp}")
+        os.system(f"cp -r {exp_path}/analysis_experiments/ /home/matteo/Dropbox/tmp_results/{exp}")
 
